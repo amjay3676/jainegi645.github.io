ad7f6e9786c9ee82d9ddf1b9c998017f684ed43a
jai <jainegi645@gmail.com>
2020-10-17 10:07:44 +0530

    added media queries

--- new.js
+++ new.js
@@ -5,3 +5,14 @@ $(function(){
   
   $('.collapse').collapse()
 
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+   // Or with jQuery
+
+   $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
